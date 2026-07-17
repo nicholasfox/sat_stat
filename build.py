fc9c55b428f67e83ec9718d43c18d835b93ba654
Nicholas Fox <nicholas@fox.com>
2026-07-17 23:45:01 +0800

    debug: use --console for visible errors

--- build.py
+++ build.py
@@ -21,6 +21,6 @@ PyInstaller.__main__.run([
     f'--add-data={os.path.join(BASE, "tle_data.json")}:.',
     '--hidden-import=cheroot.wsgi',
     '--hidden-import=webview.platforms.winforms',
-    '--windowed',
+    '--console',
     os.path.join(BASE, 'analysis.py'),
 ])
